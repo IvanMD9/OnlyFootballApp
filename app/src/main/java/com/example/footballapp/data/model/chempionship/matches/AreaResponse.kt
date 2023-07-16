fa85d2a9d47a904d6f40f23e15cb344625d6fce6
dauni <daunitisivan.999@mail.ru>
2023-07-16 12:23:40 +0300

    Добавление базовой реализации для экрана со списком команд

--- app/src/main/java/com/example/footballapp/data/model/chempionship/matches/AreaResponse.kt
+++ app/src/main/java/com/example/footballapp/data/model/chempionship/matches/AreaResponse.kt
@@ -2,7 +2,7 @@ package com.example.footballapp.data.model.chempionship.matches
 
 data class AreaResponse(
     val code: String,
-    val flag: Any,
+    val flag: Any?,
     val id: Int,
     val name: String
 )
--- app/src/main/java/com/example/footballapp/domain/model/detail_team/AreaModel.kt
+++ app/src/main/java/com/example/footballapp/domain/model/detail_team/AreaModel.kt
@@ -2,7 +2,7 @@ package com.example.footballapp.domain.model.detail_team
 
 data class AreaModel(
     val code: String,
-    val flag: Any,
+    val flag: Any?,
     val id: Int,
     val name: String
 )
