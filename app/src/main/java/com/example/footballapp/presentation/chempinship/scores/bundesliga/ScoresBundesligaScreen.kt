25c09b513ba4e287c531b0f3ab20660fb05254a9
dauni <daunitisivan.999@mail.ru>
2023-07-16 11:02:10 +0300

    Добавление базовой реализации для экрана бомбардиров

--- app/src/main/java/com/example/footballapp/presentation/chempinship/scores/bundesliga/ScoresBundesligaScreen.kt
+++ app/src/main/java/com/example/footballapp/presentation/chempinship/scores/bundesliga/ScoresBundesligaScreen.kt
@@ -1,60 +1,11 @@
 package com.example.footballapp.presentation.chempinship.scores.bundesliga
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.material.CircularProgressIndicator
-import androidx.compose.material.Divider
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
-import com.example.footballapp.presentation.chempinship.scores.components.ItemScoresInfo
+import com.example.footballapp.utils.baseui.BaseScorersWindow
 
 @Composable
-fun ScoresBundesligaScreen(
-    viewModel: ScoresBundesligaInfoViewModel = hiltViewModel()
-) {
-    val state = viewModel.state.collectAsState()
-
-    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-
-        // TODO: Добавить проверку во все экраны
-        if (state.value.data?.scorers?.isEmpty() == true) {
-            Text(
-                text = "Список пустой",
-                fontSize = 28.sp,
-                fontWeight = FontWeight.Bold,
-                color = Color.Black
-            )
-        } else {
-            LazyColumn(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .padding(20.dp)
-            ) {
-                state.value.data?.let {
-                    items(it.scorers) { result ->
-                        ItemScoresInfo(scorer = result)
-                        Divider(Modifier.height(1.dp))
-                    }
-                }
-            }
-        }
-        if (state.value.isLoading) {
-            CircularProgressIndicator(
-                modifier = Modifier.align(Alignment.Center),
-                color = Color.Black
-            )
-        }
-    }
+fun ScoresBundesligaScreen() {
+    val viewModel: ScoresBundesligaInfoViewModel = hiltViewModel()
+    BaseScorersWindow(viewModel = viewModel)
 }
---
+++ app/src/main/java/com/example/footballapp/utils/baseui/ScoresWindow.kt
@@ -0,0 +1,69 @@
+package com.example.footballapp.utils.baseui
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.Divider
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import com.example.footballapp.R
+import com.example.footballapp.domain.model.scorers.ScorersModel
+import com.example.footballapp.presentation.chempinship.scores.components.ItemScoresInfo
+import com.example.footballapp.utils.AppDimensions
+import com.example.footballapp.utils.base.BaseViewModel
+
+@Composable
+inline fun <reified ViewModel : BaseViewModel<ScorersModel>> BaseScorersWindow(
+    viewModel: ViewModel,
+    modifier: Modifier = Modifier,
+) {
+    val state = viewModel.state.collectAsState()
+
+    Box(
+        modifier = modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center,
+    ) {
+
+        if (state.value.data?.scorers?.isEmpty() == true) {
+            Text(
+                text = stringResource(id = R.string.app_scorers_empty),
+                fontSize = AppDimensions.Text.lineHeightDisplayM,
+                fontWeight = FontWeight.Bold,
+                color = colorResource(id = R.color.colorBlack),
+            )
+        } else {
+            LazyColumn(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(
+                        all = dimensionResource(id = R.dimen.horizontal_padding_large)
+                    )
+            ) {
+                state.value.data?.let { scorers ->
+                    items(scorers.scorers) { result ->
+                        ItemScoresInfo(scorer = result)
+                        Divider(Modifier.height(1.dp))
+                    }
+                }
+            }
+        }
+        if (state.value.isLoading) {
+            CircularProgressIndicator(
+                modifier = Modifier.align(Alignment.Center),
+                color = colorResource(id = R.color.colorBlack),
+            )
+        }
+    }
+}
