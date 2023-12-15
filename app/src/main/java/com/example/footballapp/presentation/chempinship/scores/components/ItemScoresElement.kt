3aff045f396077bc795c8bfc3bbc8a3da64d31ae
dauni <daunitisivan.999@mail.ru>
2023-12-15 14:00:49 +0300

    Доработан экран статистики

--- app/src/main/java/com/example/footballapp/presentation/chempinship/scores/components/ItemScoresElement.kt
+++ app/src/main/java/com/example/footballapp/presentation/chempinship/scores/components/ItemScoresElement.kt
@@ -10,84 +10,134 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import coil.compose.rememberAsyncImagePainter
 import coil.decode.SvgDecoder
 import coil.request.ImageRequest
 import com.example.footballapp.R
-import com.example.footballapp.domain.model.scorers.ScorerModel
+import com.example.footballapp.ui.theme.FootballAppTheme
 import com.example.footballapp.utils.AppDimensions
 
 @Composable
 fun ItemScoresInfo(
-    scorer: ScorerModel,
+    image: String,
+    namePlayer: String,
+    shortNameClub: String,
+    goals: Int,
+    penalties: Int,
+    assists: Int,
     modifier: Modifier = Modifier,
 ) {
-    val itemScores = rememberAsyncImagePainter(
+    val imageScores = rememberAsyncImagePainter(
         model = ImageRequest.Builder(LocalContext.current)
             .decoderFactory(SvgDecoder.Factory())
-            .data(scorer.team.crest)
+            .data(image)
             .build()
     )
     Row(
         modifier = modifier
             .fillMaxWidth()
-            .padding(
-                horizontal = 10.dp,
-                vertical = dimensionResource(id = R.dimen.vertical_padding_smallest)
-            ),
+            .padding(vertical = dimensionResource(id = R.dimen.vertical_padding_pre_small)),
         verticalAlignment = Alignment.CenterVertically,
         horizontalArrangement = Arrangement.SpaceBetween
     ) {
         Row(
+            modifier = Modifier.weight(0.6f),
             verticalAlignment = Alignment.CenterVertically,
             horizontalArrangement = Arrangement.Start
         ) {
             Image(
-                painter = itemScores,
+                painter = imageScores,
                 contentDescription = null,
                 modifier = Modifier
                     .size(
-                        size = dimensionResource(id = R.dimen.icon_size_medium)
+                        size = dimensionResource(id = R.dimen.icon_size_pre_medium)
                     )
             )
             Spacer(modifier = Modifier.width(12.dp))
             Column(horizontalAlignment = Alignment.Start) {
                 Text(
-                    text = scorer.player.name,
-                    fontSize = AppDimensions.Text.lineHeightTextS,
+                    text = namePlayer,
+                    fontSize = AppDimensions.Text.textM,
                     fontWeight = FontWeight.Medium,
                     color = colorResource(id = R.color.colorBlack),
                 )
                 Spacer(modifier = Modifier.height(4.dp))
                 Text(
-                    text = scorer.team.shortName,
-                    fontSize = AppDimensions.Text.lineHeightTextS,
+                    text = shortNameClub,
+                    fontSize = AppDimensions.Text.textM,
                     fontWeight = FontWeight.Medium,
                     color = colorResource(id = R.color.colorGray),
                 )
             }
         }
         Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .weight(0.4f),
             verticalAlignment = Alignment.CenterVertically,
-            horizontalArrangement = Arrangement.End
+            horizontalArrangement = Arrangement.SpaceBetween,
         ) {
-            Text(
-                text = "${scorer.goals}",
-                fontSize = AppDimensions.Text.lineHeightTextS,
-                fontWeight = FontWeight.Bold,
-                color = colorResource(id = R.color.colorBlack),
-            )
+            Column(modifier = Modifier.wrapContentWidth()) {
+                Text(
+                    text = stringResource(id = R.string.app_scores_goals_and_penalties),
+                    fontSize = AppDimensions.Text.textM,
+                    fontWeight = FontWeight.Light,
+                    color = colorResource(id = R.color.colorBlack),
+                )
+                Spacer(modifier = Modifier.height(4.dp))
+                Text(
+                    text = stringResource(id = R.string.app_scores_assists),
+                    fontSize = AppDimensions.Text.textM,
+                    fontWeight = FontWeight.Light,
+                    color = colorResource(id = R.color.colorBlack),
+                )
+            }
+            Spacer(modifier = Modifier.width(16.dp))
+            Column(
+                modifier = Modifier.wrapContentWidth(),
+                horizontalAlignment = Alignment.End,
+            ) {
+                Text(
+                    text = "$goals ($penalties)",
+                    fontSize = AppDimensions.Text.textM,
+                    fontWeight = FontWeight.Light,
+                    color = colorResource(id = R.color.colorBlack),
+                )
+                Spacer(modifier = Modifier.height(4.dp))
+                Text(
+                    text = "$assists",
+                    fontSize = AppDimensions.Text.textM,
+                    fontWeight = FontWeight.Light,
+                    color = colorResource(id = R.color.colorBlack),
+                )
+            }
         }
+    }
+}
+
+@Composable
+@Preview
+fun ItemScoresInfoPreview() {
+    FootballAppTheme {
+        ItemScoresInfo(
+            image = "",
+            namePlayer = "Harry Kane",
+            shortNameClub = "Bayern",
+            goals = 18,
+            penalties = 4,
+            assists = 5
+        )
     }
 }
